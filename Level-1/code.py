fecbf62901a7cb1f640d19a0319e11b3d34a5fa2
Lisa Y. Zhu <lisa.y.zhu@gmail.com>
2023-04-01 18:59:17 +0000

    Format Level-1

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,4 +1,4 @@
-'''
+"""
 ////////////////////////////////////////////////////////////
 ///                                                      ///
 ///   0. tests.py is passing but the code is vulnerable  ///
@@ -9,26 +9,26 @@
 ///   5. Compare your solution with solution.py          ///
 ///                                                      ///
 ////////////////////////////////////////////////////////////
-'''
+"""
 
 from collections import namedtuple
 
-Order = namedtuple('Order', 'id, items')
-Item = namedtuple('Item', 'type, description, amount, quantity')
+Order = namedtuple("Order", "id, items")
+Item = namedtuple("Item", "type, description, amount, quantity")
 
 
 def validorder(order: Order):
     net = 0
 
     for item in order.items:
-        if item.type == 'payment':
+        if item.type == "payment":
             net += item.amount
-        elif item.type == 'product':
+        elif item.type == "product":
             net -= item.amount * item.quantity
         else:
-            return ("Invalid item type: %s" % item.type)
+            return "Invalid item type: %s" % item.type
 
     if net != 0:
-        return ("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
-        return ("Order ID: %s - Full payment received!" % order.id)
+        return "Order ID: %s - Full payment received!" % order.id
