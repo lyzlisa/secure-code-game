2a20cbf3b087eff1293a440b4d637ecc453b21b9
Lisa Y. Zhu <lisa.y.zhu@gmail.com>
2023-04-01 18:41:07 +0000

    Remove `secrets` from requirements.txt
    `secrets` is part of standard library

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -16,18 +16,19 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+
 def validorder(order: Order):
     net = 0
-    
+
     for item in order.items:
         if item.type == 'payment':
             net += item.amount
         elif item.type == 'product':
             net -= item.amount * item.quantity
         else:
-            return("Invalid item type: %s" % item.type)
-    
+            return ("Invalid item type: %s" % item.type)
+
     if net != 0:
-        return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
+        return ("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
-        return("Order ID: %s - Full payment received!" % order.id)
+        return ("Order ID: %s - Full payment received!" % order.id)
